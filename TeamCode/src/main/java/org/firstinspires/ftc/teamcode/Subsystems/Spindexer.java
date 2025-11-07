be0b3db408a0cee6ad674753ec19edff3ea693fe
DoctorKrill08 <doctorkrill08@gmail.com>
2025-11-07 01:21:45 -0500

    Added structure for shooting motif

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Spindexer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Spindexer.java
@@ -10,6 +10,12 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @Config
 public class Spindexer {
+    public enum States{
+        RESTING,
+        INTAKING,
+        SHOOTING
+    }
+    States currentState = States.RESTING;
     ColorSensor colorSensor;
     DcMotor motor;
     public static int rotateTicks = 20;
@@ -18,6 +24,7 @@ public class Spindexer {
     int rotateNumber = 0;
     int targetBallNumber = 0;
     public boolean doneRotating = false;
+    public boolean readyToFire = false;
     LimeLight.BallColors currentColor = LimeLight.BallColors.NONE;
     LimeLight.BallColors targetColor = LimeLight.BallColors.NONE;
     LimeLight.BallColors[] motif;
@@ -29,12 +36,15 @@ public class Spindexer {
         return targetBallNumber;
     }
     public void shoot(){
+        currentState = States.SHOOTING;
         targetBallNumber = targetBallNumber + 1;
         if (targetBallNumber >= 3){
             targetBallNumber = 0;
         }
     }
     public void rotate(){
+        //Read the color 1 slot before the kicking slot
+        currentColor = ColorSensors.getBallColor(colorSensor);
         rotateNumber = rotateNumber + 1;
         motor.setTargetPosition(motor.getTargetPosition() + rotateTicks);
         if (rotateNumber >= 3){
@@ -52,7 +62,18 @@ public class Spindexer {
 
         power = error * kP;
         motor.setPower(power);
-        currentColor = ColorSensors.getBallColor(colorSensor);;
+        switch (currentState){
+            case RESTING:
+            case INTAKING:
+                readyToFire = false;
+                break;
+            case SHOOTING:
+                if (doneRotating && currentColor == targetColor){
+                    readyToFire = true;
+                    currentState = States.RESTING;
+                }
+                break;
+        }
 
         telemetry.addData("Spindexer Power", motor.getPower());
         telemetry.addData("Spindexer Target", motor.getTargetPosition());
@@ -61,5 +82,8 @@ public class Spindexer {
         telemetry.addData("Target Number",targetBallNumber);
         telemetry.addData("Ball Color",currentColor);
         telemetry.addData("Target Color",targetColor);
+        telemetry.addData("Spindexer State",currentState);
+        telemetry.addData("Spindexer readyToFire",readyToFire);
+        telemetry.addData("SpindexerDoneRotating",doneRotating);
     }
 }
